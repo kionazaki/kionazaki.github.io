b4aa2f6afe5e025c8f8c5c6b854961ef7b2bc9e6
kionazaki <kio.nazaki@gmail.com>
2016-06-04 12:19:49 +0300

    Update receiver.js

--- receiver.js
+++ receiver.js
@@ -11,8 +11,6 @@ window.onload = function() {
 
 		// Update the div element to display the message.
 		messageEle.innerHTML = e.origin+ " Message Received: " + e.data;
-		e.data.postMessage('ansveeer', '*');
-		
 		var parent = window.parent;
             	parent.postMessage('FUCK', "*");
 
